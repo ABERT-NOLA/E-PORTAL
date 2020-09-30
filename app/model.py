620fae9acf11ce1db144dc9a801cafc336a69104
Albert Obwoge <albertnyambegaa@gmail.com>
2020-09-30 13:14:52 +0300

    changes to db

--- app/model.py
+++ app/model.py
@@ -5,4 +5,17 @@ from flask_admin import Admin,AdminIndexView
 from flask_admin .contrib.sqla import ModelView
 
 class schools(db.Model):
-    __
+    __tablename__ = 'schools'
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255))
+    admin_id = db.Column(db.Integer,db.foreignkey('admin.id'))
+    def __repr__(self):
+        return f'User {self.username}'
+
+class facilitator(db.Model):
+    __tablename__ = 'Facilitator'
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255))
+    schools = db.relationship('schools',backref = 'admin',lazy ='dynamic')
+    def __repr__(self):
+        return f'User {self.username}'
