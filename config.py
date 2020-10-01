133051d0ad2a44c470187e6345c5f2c48814fae0
Alexotieno1717 <alexotieno900@gmail.com>
2020-10-01 10:56:21 +0300

    login

--- config.py
+++ config.py
@@ -1,17 +1,27 @@
 import os
 
-class Config:
 
-  
+class Config:
+    """
+    Parent configuration
+    """
+    SECRET_KEY = 'schoolvirtual'
+    UPLOADED_PHOTOS_DEST = 'app/static/photos'
+
 
 class ProdConfig(Config):
-    pass
+    """
+    Production configuration
+    """
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
 
 class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:alex17176251@localhost/portal'
     DEBUG = True
 
+
 config_options = {
-'development':DevConfig,
-'production':ProdConfig
+    'development': DevConfig,
+    'production': ProdConfig
 }
